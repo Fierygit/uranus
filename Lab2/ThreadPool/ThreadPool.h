58fcbf38e61a48e2a41649d7090e7c856763455e
JameyWoo <2622075127wjh@gmail.com>
2020-04-13 08:58:02 +0800

    add ThreadPool

---
+++ Lab2/ThreadPool/ThreadPool.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <pthread.h>
+#include <deque>
+#include <functional>  // for std::function, std::bind
+
+// 每一个job都是一个function对象
+namespace uranus {
+class ThreadPool {
+   public:
+   // 一个任务是一个函数
+	typedef std::function<void()> Task;
+
+   public:
+	ThreadPool(int threadNum = 10);
+	~ThreadPool();
+
+   public:
+	size_t addTask(const Task& task);
+	void stop();
+	int size();
+	Task take();
+
+   private:
+	int createThreads();
+	static void* threadFunc(void* threadData);
+
+   private:
+	ThreadPool& operator=(const ThreadPool&);
+	ThreadPool(const ThreadPool&);
+
+   private:
+	volatile bool isRunning_;
+	int threadsNum_;
+	pthread_t* threads_;
+
+	// 任务队列
+	std::deque<Task> taskQueue_;
+	pthread_mutex_t mutex_;
+	pthread_cond_t condition_;
+};
+}  // namespace uranus
