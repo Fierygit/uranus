56bd2dd01b8ad5336df7aeabea435b680acbc991
JameyWoo <2622075127wjh@gmail.com>
2020-04-18 22:16:05 +0800

    'main'

--- Lab2/epoll/main.cpp
+++ Lab2/epoll/main.cpp
@@ -3,8 +3,11 @@
 
 
 int main() {
-    http_server server;
-    server.start();
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    http_server* server = new http_server(7777);
+    server->start();
+    std::cout << "shutdown!\n";
+//    http_server server;
+//    server.start();
+//    std::cout << "Hello, World!" << std::endl;
+//    return 0;
 }
