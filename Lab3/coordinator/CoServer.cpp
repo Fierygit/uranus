bdcfe43f5f3a516a7d0b3fa05d19319bbf5cd66e
firefly <531109985@qq.com>
2020-05-10 09:59:52 +0800

    init thread

--- Lab3/coordinator/CoServer.cpp
+++ Lab3/coordinator/CoServer.cpp
@@ -9,18 +9,35 @@
 #include "../common/loguru.hpp"
 
 void CoServer::run() {
+    for (;;) {
+    }
 
 }
 
+
 CoServer CoServer::init() {
     std::cout << welcomeLogo << std::endl;
-    LOG_F(INFO,"addr: %s, port: %d",addr.c_str());
+    LOG_F(INFO, "addr: %s, port: %d", addr.c_str());
 
-    auto func = []() {
+    /*
+     * 创建子线程接受新的 pantipant 请求
+     */
+    auto acceptParticipant = [this]() {
         std::cout << "hi" << std::endl;
     };
-    std::thread t{func};
-    t.detach();
+    std::thread paThread{acceptParticipant};
+    paThread.detach();
+
+    /*
+     * 创建子线程接受新的 client 连接
+     */
+    auto acceptClient = [this]() {
+        std::cout << "hi" << std::endl;
+    };
+    std::thread acThread{acceptParticipant};
+    acThread.detach();
+
+
     return *this;
 }
 
