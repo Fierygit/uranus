1c9e33e070e61f4fee123f5e8022ef926d47b164
JameyWoo <2622075127wjh@gmail.com>
2020-05-10 14:07:28 +0800

    实现生产者消费者

--- Lab3/coordinator/BoundedBlockingQueue.h
+++ Lab3/coordinator/BoundedBlockingQueue.h
@@ -5,6 +5,7 @@
 #include <assert.h>
 #include <mutex>
 #include <condition_variable>
+#include <vector>
 
 
 /*
@@ -14,37 +15,85 @@
 template<typename T>
 class BoundedBlockingQueue {
 public:
-    explicit BoundedBlockingQueue(int maxSize) {
-        std::cout << maxSize << std::endl;
+    // 默认
+    explicit BoundedBlockingQueue() : BoundedBlockingQueue(1024) {}
+
+    explicit BoundedBlockingQueue(int max_size) {
+        capacity_ = max_size;
+        queue_.resize(capacity_);
+
+        size_ = 0;
+        put_idx_ = 0;
+        get_idx_ = 0;
     }
 
 
-    void put(const T &x) {}
+    void put(const T &x) {
+        // 在unique_lock对象的声明周期内，它所管理的锁对象会一直保持上锁状态
+        // unique_lock 具有 lock_guard 的所有功能，而且更为灵活
+        std::unique_lock<std::mutex> lck(mutex_);
+        while (size_ == capacity_) {
+            // 传 引用
+            notFull.wait(std::ref(lck));
+        }
+        queue_[put_idx_] = x;
+        put_idx_++;
+        size_++;
+        put_idx_ = put_idx_ % capacity_;
+        // 告诉消费者可以消费了
+        notEmpty.notify_one();
+        // 此时可以解锁
+        lck.unlock();
+    }
 
-    void put(T &&x) {}
+    void put(T &&x) {
+        put(x);
+    }
 
-    T take() {}
+    T get() {
+        std::unique_lock<std::mutex> lck(mutex_);
+        while (size_ == 0) {
+            notEmpty.wait(std::ref(lck));
+        }
+        T val = queue_[get_idx_];
+        get_idx_++;
+        size_--;
+        get_idx_ = get_idx_ % capacity_;
+        notFull.notify_one();
+        lck.unlock();
+        return val;
+    }
 
-    bool empty() const {}
+    bool empty() const {
+        return size_ == 0;
+    }
 
-    bool full() const {}
+    bool full() const {
+        return size_ == capacity_;
+    }
 
-    size_t size() const {}
+    size_t size() const {
+        return size_;
+    }
 
     //
     size_t capacity() const {
-        std::lock_guard<std::mutex> lock_guard(mutex);
-        return 1;
+        return capacity_;
     }
 
 private:
-    //锁
-    std::mutex mutex{};
-    //条件变量
-    std::condition_variable notEmpty;
-    std::condition_variable notFull;
+    // 锁
+    std::mutex mutex_{};
+    // 条件变量
+    std::condition_variable notEmpty;  // 在 get 中使用
+    std::condition_variable notFull;  // 在 put 中使用
 
-
+private:
+    size_t capacity_;  // 容量
+    size_t size_;  // 实际的大小
+    std::vector<T> queue_;  // 存储数据
+    int put_idx_;  // 放 下一个数据 的索引
+    int get_idx_;  // 取 当前数据 的索引
 };
 
 
--- Lab3/test/testBlockingqueue.cpp
+++ Lab3/test/testBlockingqueue.cpp
@@ -2,11 +2,54 @@
 // Created by Firefly on 2020/5/10.
 //
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <cassert>
+
 #include "../coordinator/BoundedBlockingQueue.h"
+#include "../common/loguru.hpp"
+
+std::vector<int> testVec;
+
+void putTest(BoundedBlockingQueue<int>& queueTest, int x) {
+    for (int i = 0; i < x; i++) {
+//        定时
+//        std::this_thread::sleep_for(std::chrono::milliseconds (5));
+        queueTest.put(i);
+    }
+}
+
+void getTest(BoundedBlockingQueue<int>& queueTest, int x) {
+    for (int i = 0; i < x; i++) {
+//         定时
+//        std::this_thread::sleep_for(std::chrono::milliseconds(4));
+        auto get_val = queueTest.get();
+        testVec.push_back(get_val);
+//        std::cout << get_val << std::endl;
+    }
+}
 
 
 int main() {
-    BoundedBlockingQueue<int> give_me_a_name(12);
-    std::cout << "he";
+    BoundedBlockingQueue<int> queueTest(10);
 
+    // 测试的数据量
+    int test_size = 1000;
+
+    std::thread putThread(putTest, std::ref(queueTest), test_size);
+    std::thread getThread(getTest, std::ref(queueTest), test_size);
+
+    putThread.join();
+    getThread.join();
+
+    // 取出的数量是否正确
+    assert(testVec.size() == test_size);
+    
+    for (int i = 0; i < testVec.size() - 1; i++) {
+        // 取出的数据顺序是否正确
+        assert(testVec[i + 1] - testVec[i] == 1);
+    }
+
+    std::cout << "test pass..." << std::endl;
+    return 0;
 }
